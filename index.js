55120e12ea435977acf8df215461ee56d1a92a3d
Seth Tippetts <sethtippetts@gmail.com>
2017-01-20 19:53:45 -0700

    Got rid of server director (moving to Firebase)

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const firebase = require('firebase');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAwzjfVGXuoD2cHc6qh6n2ESMozZtN7Cnw",
+  authDomain: "simple-chat-ab999.firebaseapp.com",
+  databaseURL: "https://simple-chat-ab999.firebaseio.com",
+  storageBucket: "simple-chat-ab999.appspot.com",
+  messagingSenderId: "393866197849"
+};
+firebase.initializeApp(config);
